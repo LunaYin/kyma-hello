a02d88f8f65458c7d1d29252aae1772ad1acf860
LunaYin <39580000+LunaYin@users.noreply.github.com>
2021-12-30 13:42:39 +0800

    Delete kyme-hello.js

--- kyme-hello.js
+++
@@ -1,8 +0,0 @@
-var path = require('path');
-module.exports = {
-    main: function (event, context) {
-        console.log('Join: ' + path.join('kyma-hello'+'example'));
-        return "Hello world";
-    }
-}
-
